4d152d9f8e2fbb2c1fd12c83284f7e6590f31bfc
isumizumi <isumi.karina72@gmail.com>
2021-08-27 21:04:09 +0700

    revisi SearchGifForm

--- src/pages/SearchGifForm.jsx
+++ src/pages/SearchGifForm.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
 import Loader from "./Loader";
 
